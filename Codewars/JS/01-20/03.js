56f1069808feb4706fb4a03641efc64508427b9b
Alexander Avdienko <104214933+aavdienko@users.noreply.github.com>
2022-12-17 14:47:58 +0100

    Check same case

---
+++ Codewars/JS/01-20/03.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/train/javascript
+
+// Write a function that will check if two given characters are the same case.
+
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+// Examples
+
+// 'a' and 'g' returns 1
+
+// 'A' and 'C' returns 1
+
+// 'b' and 'G' returns 0
+
+// 'B' and 'g' returns 0
+
+// '0' and '?' returns -1
+
+const isNotLetter = s => s === s.toUpperCase() && s === s.toLowerCase()
+const defineCase = s => s === s.toUpperCase() ? 'Upper' : 'Lower'
+
+function sameCase(a, b){
+  if (isNotLetter(a) || isNotLetter(b)) return -1
+  return defineCase(a) === defineCase(b) ? 1 : 0
+} 
