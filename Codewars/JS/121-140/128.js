b24d39395268ef41634da1b89a1af8571a84a1d8
Alexander Avdienko <104214933+aavdienko@users.noreply.github.com>
2023-07-20 18:33:26 +0200

    Who is going to pay for the wall?

--- Codewars/JS/121-140/128.js
+++ Codewars/JS/121-140/128.js
@@ -1,12 +1,3 @@
 function whoIsPaying(name){
-  //your code here
-  console.log(name)
-  const arr = [name]
-  if (!name.length) return ['']
-  
-  if (name.length <= 2) {
-    return arr
-  }
-  
-  return [...arr, name.slice(0,2)]
+  return name.length > 2 ? [name, name.slice(0,2)] : [name]
 }
